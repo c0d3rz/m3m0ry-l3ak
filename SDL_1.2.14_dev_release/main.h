96ddcb6409078e7f46e003471c058445c079a5be
c0d3rz <war10ckblogspot@gmail.com>
2012-07-04 02:07:11 +0530

    SDL v1 Release

---
+++ SDL_1.2.14_dev_release/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#ifdef __cplusplus
+    #include <iostream>
+    #include <cstdlib>
+#else
+    #include <stdio.h>
+    #include <stdlib.h>
+#endif
+
+#ifdef __APPLE__
+    #include <SDL/SDL.h>
+#else
+    #include <SDL.h>
+#endif
+
+#endif // MAIN_H_INCLUDED
